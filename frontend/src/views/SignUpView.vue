267eea6bbc40c9a2df5f2dd93f1fe9a4324d8756
JCGViljoen <jcgviljone51@gmail.com>
2023-09-05 14:35:34 +0200

    did login form

--- frontend/src/views/SignUpView.vue
+++ frontend/src/views/SignUpView.vue
@@ -1,11 +1,16 @@
 <template>
     <div>
-
+        <h1>Sign up</h1>
+        <Signup/>
     </div>
 </template>
 
 <script>
+import Signup from '@/components/SignupComp.vue'
     export default {
+        components: {
+            Signup
+        }
         
     }
 </script>
