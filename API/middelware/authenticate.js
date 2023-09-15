1373db746b44f42d7924aa8692291d1994031d46
JCGViljoen <jcgviljone51@gmail.com>
2023-09-15 09:03:18 +0200

    fixed secret key

--- API/middelware/authenticate.js
+++ API/middelware/authenticate.js
@@ -6,7 +6,7 @@ function createToken(user) {
       emailAdd: user.emailAdd,
       userPass: user.userPass,
     },
-    process.env.SECRET_KEY,
+    process.env.secret_keygit,
     {
       expiresIn: "1h",
     }
