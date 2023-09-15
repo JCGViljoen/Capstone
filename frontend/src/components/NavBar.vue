6ec50ab08126dc5c9d7286ebf19dc145050593ab
JCGViljoen <jcgviljoen51@gmail.com>
2023-09-15 18:27:44 +0200

    navbar

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,92 +1,62 @@
 <template>
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="collapse" id="navbarToggleExternalContent" data-bs-theme="dark">
+    <div class="bg-dark p-4">
+      <h5 class="text-body-emphasis h4">DIGI-STOP</h5>
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <router-link to="/" class="nav-link" id="home-nav" aria-current="page">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/about" class="nav-link">About</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/products" class="nav-link">Products</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/admin" class="nav-link">Admin</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link :to="'/user/' + userId" class="nav-link">Profile</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/signup" class="nav-link">Sign Up</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/login" class="nav-link">Login</router-link>
+        </li>
+      </ul>
+      <router-link to="/cart" class="nav-link">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+          <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+        </svg>
+      </router-link>
+      <router-link to="/checkout" class="nav-link">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag-dash-fill" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M10.5 3.5a2.5 2.5 0 0 0-5 0V4h5v-.5zm1 0V4H15v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4h3.5v-.5a3.5 3.5 0 1 1 7 0zM6 9.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1H6z"/>
+        </svg>
+      </router-link>
+    </div>
+  </div>
+  <nav class="navbar navbar-dark bg-dark">
     <div class="container-fluid">
-      <router-link class="navbar-brand logo-animation" to="/">DIGI-STOP</router-link>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
+      <router-link to="/" class="navbar-brand logo-animation">
+        DIGI-STOP
+      </router-link>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <router-link to="/" class="nav-link" id="home-nav" aria-current="page">
-              Home
-            </router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" to="/about">
-              About
-            </router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" to="/products">
-              Products
-            </router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" to="/admin">
-              Admin
-            </router-link>
-          </li>
-          <li>
-            <router-link class="nav-link" to="/user/:id">
-              Profile
-            </router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" to="/signup">
-              Sign Up
-            </router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" to="/login">
-              Login
-            </router-link>
-          </li>
-        </ul>
-        <router-link class="nav-link" to="/cart">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            class="bi bi-cart"
-            viewBox="0 0 16 16"
-          >
-            <path
-              d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"
-            />
-          </svg>
-        </router-link>
-        <router-link class="nav-link" to="/checkout">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            class="bi bi-bag-dash-fill"
-            viewBox="0 0 16 16"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M10.5 3.5a2.5 2.5 0 0 0-5 0V4h5v-.5zm1 0V4H15v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4h3.5v-.5a3.5 3.5 0 1 1 7 0zM6 9.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1H6z"
-            />
-          </svg>
-        </router-link>
-      </div>
     </div>
   </nav>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      userId: 1 
+    };
+  }
+}
 </script>
 
 <style scoped>
@@ -99,6 +69,15 @@ nav {
   position: sticky;
   height: 10vh;
 }
+.logo-animation {
+  color: red;
+  transition: transform 0.3s ease-in-out, color 0.3s ease-in-out;
+}
+
+.logo-animation:hover {
+  transform: scale(1.2);
+  color: #fff;
+}
 
 .navbar-nav {
   text-align: center;
@@ -109,7 +88,6 @@ nav {
   position: relative;
   text-decoration: none;
   transition: color 0.3s ease-in-out;
-  
 }
 
 .nav-link:hover {
@@ -128,7 +106,7 @@ nav {
 }
 
 .navbar-toggler-icon {
-  background-color: red; /* Change the hamburger icon color to white */
+  background-color: red;
 }
 
 @media screen and (max-width: 300px) {
@@ -148,9 +126,7 @@ nav {
 @media screen and (max-width: 991px) {
   .nav-link {
     color: red;
-    text-decoration: underline seashell; 
+    text-decoration: underline seashell;
   }
 }
-
-
 </style>
